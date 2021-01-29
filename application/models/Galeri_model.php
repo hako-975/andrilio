77de1533c0614a4d394b4b9531427363c3c54b93
hako-975 <andrifirmansaputra1@gmail.com>
2021-01-29 22:43:41 +0800

    update serv

--- application/models/Galeri_model.php
+++ application/models/Galeri_model.php
@@ -18,7 +18,7 @@ class Galeri_model extends CI_Model
 	// ------------------ START INSERT ------------------
 	private function _set_upload_options(){
 		$config = [];
-		$config['upload_path'] 		= './assets/img/img_galeri/'; 
+		$config['upload_path'] 		= realpath(FCPATH . '/assets/img/img_galeri/'); 
 		$config['remove_spaces']	= TRUE;
 		$config['encrypt_name'] 	= TRUE; 
 		$config['allowed_types'] 	= 'gif|jpg|png|jpeg|GIF|JPG|PNG|JPEG';
@@ -76,7 +76,7 @@ class Galeri_model extends CI_Model
 
 		$img_galeri = $_FILES['img_galeri']['name'];
 		if ($img_galeri) {
-			$config['upload_path'] = './assets/img/img_galeri/';
+			$config['upload_path'] = realpath(FCPATH . '/assets/img/img_galeri/');
 			$config['allowed_types'] = 'gif|jpg|png|jpeg';
 		
 			$this->load->library('upload', $config);
